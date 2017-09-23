1d603e8da08fb18ed13fbe02151593576fb3ced5
hughy603 <rice.adam@gmail.com>
2017-09-22 22:42:52 -0400

    Added other stuff

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello world')
+""" Entry point into the blackjack calculation program
+"""
+import sys
+from cards.deck import Deck
+
+def main():
+    deck = Deck(2)
+    print(len(deck), 'cards')
+
+if __name__ == '__main__':
+    sys.exit(main())
